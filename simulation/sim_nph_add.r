a75d0e7fcafc27a2ac634f7ab28012e34c2cca62
Yuan Zhang <63809399+YZhang469@users.noreply.github.com>
2024-11-28 17:53:50 -0600

    Update sim_nph_add.r

--- simulation/sim_nph_add.r
+++ simulation/sim_nph_add.r
@@ -8,12 +8,6 @@ res11 <- sim(ph = FALSE, n.sim = 1000, n = 2500, K = 5, interval = 10, L = 50,
 
 write.csv(res11, "sim_nph_add_2500.csv")
 
-res11 <- sim(ph = FALSE, n.sim = 1000, n = 5000, K = 5, interval = 10, L = 50, 
-             a = 1, b = 50, lambda0C = 0.0014, lambda0T = 0.0028, 
-             method = "add")
-
-write.csv(res11, "sim_nph_add_5000.csv")
-
 set.seed(232)
 
 res15 <- sim(ph = FALSE, n.sim = 1000, n = 2500, K = 5, interval = 10, L = 50, 
@@ -21,9 +15,3 @@ res15 <- sim(ph = FALSE, n.sim = 1000, n = 2500, K = 5, interval = 10, L = 50,
              method = "add")
 
 write.csv(res15, "sim_nph_add_highC_2500.csv")
-
-res15 <- sim(ph = FALSE, n.sim = 1000, n = 5000, K = 5, interval = 10, L = 50, 
-             a = 1, b = 50, lambda0C = 0.004, lambda0T = 0.0075, 
-             method = "add")
-
-write.csv(res15, "sim_nph_add_highC_5000.csv")
