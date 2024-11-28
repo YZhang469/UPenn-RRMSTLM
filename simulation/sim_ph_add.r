837b6f9a5c6ced038cdd8e05c74cf07233bae609
Yuan Zhang <63809399+YZhang469@users.noreply.github.com>
2024-11-28 17:55:55 -0600

    Update sim_ph_add.r

--- simulation/sim_ph_add.r
+++ simulation/sim_ph_add.r
@@ -6,7 +6,7 @@ res3 <- sim(ph = TRUE, n.sim = 1000, n = 2500, K = 5, interval = 10, L = 50,
             a = 1, b = 50, lambda0C = 0.0012, lambda0T = 0.0024, 
             method = "add")
 
-write.csv(res3, "sim_ph_add_2500.csv")
+write.csv(res3, "sim_ph_add_lowC.csv")
 
 set.seed(132)
 
@@ -14,4 +14,4 @@ res7 <- sim(ph = TRUE, n.sim = 1000, n = 2500, K = 5, interval = 10, L = 50,
             a = 1, b = 50, lambda0C = 0.004, lambda0T = 0.0077, 
             method = "add")
 
-write.csv(res7, "sim_ph_add_highC_2500.csv")
+write.csv(res7, "sim_ph_add_highC.csv")
