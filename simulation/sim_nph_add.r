2cd03b5032cd04edea928224c63978c9eba268b6
Yuan Zhang <63809399+YZhang469@users.noreply.github.com>
2024-11-28 17:48:06 -0600

    Create sim_nph_add.r

---
+++ simulation/sim_nph_add.r
@@ -0,0 +1,37 @@
+source("function.r")
+
+set.seed(231)
+
+res11 <- sim(ph = FALSE, n.sim = 1000, n = 2500, K = 5, interval = 10, L = 50, 
+             a = 1, b = 50, lambda0C = 0.0014, lambda0T = 0.0028, 
+             method = "add")
+
+write.csv(res11, "sim_nph_add_2500.csv")
+
+res11
+
+res11 <- sim(ph = FALSE, n.sim = 1000, n = 5000, K = 5, interval = 10, L = 50, 
+             a = 1, b = 50, lambda0C = 0.0014, lambda0T = 0.0028, 
+             method = "add")
+
+write.csv(res11, "sim_nph_add_5000.csv")
+
+res11
+
+set.seed(232)
+
+res15 <- sim(ph = FALSE, n.sim = 1000, n = 2500, K = 5, interval = 10, L = 50, 
+             a = 1, b = 50, lambda0C = 0.004, lambda0T = 0.0075, 
+             method = "add")
+
+write.csv(res15, "sim_nph_add_highC_2500.csv")
+
+res15
+
+res15 <- sim(ph = FALSE, n.sim = 1000, n = 5000, K = 5, interval = 10, L = 50, 
+             a = 1, b = 50, lambda0C = 0.004, lambda0T = 0.0075, 
+             method = "add")
+
+write.csv(res15, "sim_nph_add_highC_5000.csv")
+
+res15
