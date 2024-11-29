4c4905f2d068a95a242a6ccf8bd33ac11beed8f0
Yuan Zhang <63809399+YZhang469@users.noreply.github.com>
2024-11-28 18:15:15 -0600

    Update analysis.r

--- application/analysis.r
+++ application/analysis.r
@@ -172,10 +172,6 @@ CSk <- as.Date(do.call(paste0, expand.grid(day, year)), format = "%m/%d/%Y") -
   as.Date("01/01/1960", format = "%m/%d/%Y")
 res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
                        ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
-                       CSk = CSk, L = 1*365, method = "add")
-write.csv(res.add, "analysis_add_month_1y.csv")
-res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
-                       ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
                        CSk = CSk, L = 30, method = "add")
 write.csv(res.add, "analysis_add_month_1m.csv")
 res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
@@ -186,46 +182,39 @@ res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtIne
                        ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
                        CSk = CSk, L = 180, method = "add")
 write.csv(res.add, "analysis_add_month_6m.csv")
+res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
+                       ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
+                       CSk = CSk, L = 1*365, method = "add")
+write.csv(res.add, "analysis_add_month_1y.csv")
 
 # weekly
 # CSk <- sum.dts["Min."] + (1:floor((sum.dts["Max."]-sum.dts["Min."]-365)/7)) * 7
 CSk <- sum.dts["Min."] + (1:520) * 7
-# res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
-#                        ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
-#                        CSk = CSk, L = 1*365, method = "stratified", stratified = "add")
-# write.csv(res.add, "analysis_add_week.csv")
-# res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
-#                        ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
-#                        CSk = CSk, L = 30, method = "stratified", stratified = "add")
-# write.csv(res.add, "analysis_add_week_1m.csv")
 res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
                        ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
-                       CSk = CSk, L = 90, method = "stratified", stratified = "add")
-write.csv(res.add, "analysis_add_week_3m_Sik.csv")
-# res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
-#                        ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
-#                        CSk = CSk, L = 180, method = "stratified", stratified = "add")
-# write.csv(res.add, "analysis_add_week_6m.csv")
-# res.multi <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
-#                          ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
-#                          CSk = CSk, L = 1*365, method = "stratified", stratified = "multi")
-# write.csv(res.multi, "analysis_multi_week.csv")
+                       CSk = CSk, L = 30, method = "add")
+write.csv(res.add, "analysis_add_week_1m.csv")
+res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
+                       ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
+                       CSk = CSk, L = 90, method = "add")
+write.csv(res.add, "analysis_add_week_3m.csv")
+res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
+                       ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
+                       CSk = CSk, L = 180, method = "add")
+write.csv(res.add, "analysis_add_week_6m.csv")
+res.add <- analyzeData(dat = study0, datA = hist1, IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
+                       ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
+                       CSk = CSk, L = 1*365, method = "add")
+write.csv(res.add, "analysis_add_week_1y.csv")
 
 # quarterly
-# day <- c("3/31/", "6/30/", "9/30/", "12/31/")
-# year <- 2010:(2010+floor((sum.dts["Max."]-sum.dts["Min."]+1)/365)-1-1)
-# CSk <- as.Date(do.call(paste0, expand.grid(day, year)), format = "%m/%d/%Y") - 
-#   as.Date("01/01/1960", format = "%m/%d/%Y")
-# res.multi <- analyzeData(dat = study0, datA = hist1, 
-#                          IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
-#                          ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, 
-#                          Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
-#                          CSk = CSk, L = 180, method = "stratified", stratified = "multi")
-# write.csv(res.multi, "analysis_multi_quarter.csv")
-# Znames.TV <- c("dialysis", "ascites", "enceph", "albumin", "bili", "creat", "inr", "sodium", "Sik")
-# res.multi <- analyzeData(dat = study0, datA = hist1, 
-#                          IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
-#                          ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, 
-#                          Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
-#                          CSk = CSk, L = 180, method = "stratified", stratified = "multi")
-# write.csv(res.multi, "analysis_multi_quarter_Sik.csv")
+day <- c("3/31/", "6/30/", "9/30/", "12/31/")
+year <- 2010:(2010+floor((sum.dts["Max."]-sum.dts["Min."]+1)/365)-1-1)
+CSk <- as.Date(do.call(paste0, expand.grid(day, year)), format = "%m/%d/%Y") - 
+  as.Date("01/01/1960", format = "%m/%d/%Y")
+res.add <- analyzeData(dat = study0, datA = hist1, 
+                       IDname = "WL_ID_CODE", TrtInelname = "inactive", Ename = "dt_wl", 
+                       ZCnames = ZCnames, ZTnames.TI = ZTnames.TI, ZTnames.TV = ZTnames.TV, 
+                       Znames.TI = Znames.TI, Znames.TV = Znames.TV, 
+                       CSk = CSk, L = 90, method = "add")
+write.csv(res.add, "analysis_add_quarter.csv")
